e90a9a072e69cf7e5fcd926a7bc657ab25357d81
pzf0000 <pzf0000@foxmail.com>
2018-03-20 15:42:10 +0800

    create django project

--- SchoolOfSoftware/settings.py
+++ SchoolOfSoftware/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x7qi*_08(6#goiqwf3%l4fivq3fq4x5t9xkny+huf)teh7gj%!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_jinja',
+
     'rest_framework',
+    'django_jinja',
     'django_filters',
+
+    'UserManagement',
+    'Pages',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +68,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.static',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -121,4 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/templates/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/templates/static/')
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'templates/static'),
+# )
