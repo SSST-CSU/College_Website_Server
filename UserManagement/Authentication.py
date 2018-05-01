18f5005f6dcd2875984a61453a36065dd144d4a5
pzf0000 <pzf0000@foxmail.com>
2018-05-02 00:24:32 +0800

    修改主页，并完成了主页的登录功能

---
+++ UserManagement/Authentication.py
@@ -0,0 +1,18 @@
+from UserManagement.models import *
+
+
+def AuthenticateUser(user_id, user_pwd):
+    """
+    用户认证，根据用户id和密码验证
+    :param user_id: 用户id（学号、工号）
+    :param user_pwd: 密码
+    :return: User对象，若验证失败则返回None
+    """
+    user = None
+    try:
+        user = User.objects.get(id=user_id)
+        if user.pwd != user_pwd:
+            user = None
+    except:
+        user = None
+    return user
--- UserManagement/views.py
+++ UserManagement/views.py
@@ -1,3 +1,40 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from UserManagement.models import User
+import json
 
-# Create your views here.
+
+def login(request):
+    id = request.POST['user_id']
+    pwd = request.POST['user_pwd']
+    msg = 0
+    try:
+        user = User.objects.get(id=id)
+        if pwd == user.pwd:
+            msg = 2
+            request.session['user_id'] = user.id
+            request.session['user_pwd'] = user.pwd
+        else:
+            msg = 1
+    except:
+        msg = -1
+    ret = {
+        "msg": msg
+    }
+    return HttpResponse(json.dumps(ret))
+
+
+def logout(request):
+    id = request.POST['user_id']
+    msg = 0
+    try:
+        user = User.objects.get(id=id)
+        request.session['user_id'] = None
+        request.session['user_pwd'] = None
+        msg = 1
+    except:
+        pass
+    ret = {
+        "msg": msg
+    }
+    return HttpResponse(json.dumps(ret))
