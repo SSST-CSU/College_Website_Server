2677415762bdcf89f19357a16c1d7a1b37c8375a
pzf0000 <pzf0000@foxmail.com>
2018-06-17 15:22:52 +0800

    完善用户管理模块Student

--- UserManagement/views/student_view.py
+++ UserManagement/views/student_view.py
@@ -11,7 +11,55 @@ from .user_view import UserViewSet
 
 import json
 
+
+class MajorViewSet(ModelViewSet):
+    serializers_class = Major
+    queryset = Major.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        """
+        查看
+        """
+        perm_set = request.session['perm_set']
+        if 'UserManagement.view_major' in perm_set:
+            return super(MajorViewSet, self).list(request, *args, **kwargs)
+        else:
+            data = {'detail': '没有权限'}
+            return Response(data, status=status.HTTP_403_FORBIDDEN)
+
+    def create(self, request, *args, **kwargs):
+        """
+        增加
+        """
+        perm_set = request.session['perm_set']
+        if 'UserManagement.create_major' in perm_set:
+            return super(MajorViewSet, self).create(request, *args, **kwargs)
+        else:
+            data = {'detail': '没有权限'}
+            return Response(data, status=status.HTTP_403_FORBIDDEN)
+
+    def update(self, request, *args, **kwargs):
+        """
+        修改
+        """
+        perm_set = request.session['perm_set']
+        if 'UserManagement.update_major' in perm_set:
+            return super(MajorViewSet, self).update(request, *args, **kwargs)
+        else:
+            data = {'detail': '没有权限'}
+            return Response(data, status=status.HTTP_403_FORBIDDEN)
+
+    def destroy(self, request, *args, **kwargs):
+        """
+        删除
+        """
+        perm_set = request.session['perm_set']
+        if 'UserManagement.delete_major' in perm_set:
+            return super(MajorViewSet, self).destroy(request, *args, **kwargs)
+        else:
+            data = {'detail': '没有权限'}
+            return Response(data, status=status.HTTP_403_FORBIDDEN)
+
+
 class UndergraduateStudentViewSet(UserViewSet):
     pass
-
-
