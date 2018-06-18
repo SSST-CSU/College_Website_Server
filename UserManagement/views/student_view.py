3936c6d10230440bd04aa8e46e60d87f812a2458
pzf0000 <pzf0000@foxmail.com>
2018-06-18 23:24:03 +0800

    完善用户管理模块Student，完善view

--- UserManagement/views/student_view.py
+++ UserManagement/views/student_view.py
@@ -61,5 +61,54 @@ class MajorViewSet(ModelViewSet):
             return Response(data, status=status.HTTP_403_FORBIDDEN)
 
 
+class StudentGradeViewSet(ModelViewSet):
+    serializers_class = Major
+    queryset = Major.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        """
+        查看
+        """
+        perm_set = request.session['perm_set']
+        if 'UserManagement.view_major' in perm_set:
+            return super(StudentGradeViewSet, self).list(request, *args, **kwargs)
+        else:
+            data = {'detail': '没有权限'}
+            return Response(data, status=status.HTTP_403_FORBIDDEN)
+
+    def create(self, request, *args, **kwargs):
+        """
+        增加
+        """
+        perm_set = request.session['perm_set']
+        if 'UserManagement.create_major' in perm_set:
+            return super(StudentGradeViewSet, self).create(request, *args, **kwargs)
+        else:
+            data = {'detail': '没有权限'}
+            return Response(data, status=status.HTTP_403_FORBIDDEN)
+
+    def update(self, request, *args, **kwargs):
+        """
+        修改
+        """
+        perm_set = request.session['perm_set']
+        if 'UserManagement.update_major' in perm_set:
+            return super(StudentGradeViewSet, self).update(request, *args, **kwargs)
+        else:
+            data = {'detail': '没有权限'}
+            return Response(data, status=status.HTTP_403_FORBIDDEN)
+
+    def destroy(self, request, *args, **kwargs):
+        """
+        删除
+        """
+        perm_set = request.session['perm_set']
+        if 'UserManagement.delete_major' in perm_set:
+            return super(StudentGradeViewSet, self).destroy(request, *args, **kwargs)
+        else:
+            data = {'detail': '没有权限'}
+            return Response(data, status=status.HTTP_403_FORBIDDEN)
+
+
 class UndergraduateStudentViewSet(UserViewSet):
     pass
